7e5fbd086dece9c6c9de7f6c3f018dbda3eeb385
badjateamisha <amishabadjate175@gmail.com>
2022-05-09 10:19:15 +0530

    Maximum

--- MaximumOfThree/MaximumOfThree/Program.cs
+++ MaximumOfThree/MaximumOfThree/Program.cs
@@ -1,12 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MaximumOfThree;
+
+
 Console.WriteLine("Welcome to Maximum of Three using generics!");
 
-Console.WriteLine("Please enter the values of integers x, y and z:");
-int x = Convert.ToInt16(Console.ReadLine());
-int y = Convert.ToInt16(Console.ReadLine());
-int z = Convert.ToInt16(Console.ReadLine());
-UsingInt usingInt = new UsingInt();
-int result = usingInt.Maximum(x, y, z);
-Console.WriteLine("The Maximum of three integers is :" + result);
+while (true)
+{
 
+    Console.WriteLine("Choose the Program to be executed :\n1)using INT\n2)using Float\n3)usingString\n4)using generic class\n5)using generic class&Method\n6)using array");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine("Please enter the values of integers x, y and z:");
+            int x = Convert.ToInt16(Console.ReadLine());
+            int y = Convert.ToInt16(Console.ReadLine());
+            int z = Convert.ToInt16(Console.ReadLine());
+            UsingInt usingInt = new UsingInt();
+            int result = usingInt.Maximum(x, y, z);
+            Console.WriteLine("The Maximum of three integers is :" + result);
+            Console.WriteLine("\n");
+            break;
+        case 2:
+            Console.WriteLine("Please enter the values of floats a, b and c:");
+            double a = Convert.ToInt16(Console.ReadLine());
+            double b = Convert.ToInt16(Console.ReadLine());
+            double c = Convert.ToInt16(Console.ReadLine());
+            UsingFloat Usingfloat = new UsingFloat();
+            double result1 = Usingfloat.Maximumfloat(a, b, c);
+            Console.WriteLine("The Maximum of three integers is :" + result1);
+            Console.WriteLine("\n");
+            break;
+    }
+}
---
+++ MaximumOfThree/MaximumOfThreeTests/UsingFloatTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MaximumOfThree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumOfThree.Tests
+{
+    [TestClass()]
+    public class UsingFloatTests
+    {
+        [TestMethod()]
+        public void MaximumfloatTest()
+        {
+            UsingFloat Usingfloat = new UsingFloat();
+            double result = Usingfloat.Maximumfloat(6.6, 5.5, 4.4);
+            Assert.AreEqual(6.6, result);
+
+        }
+        [TestMethod()]
+        public void MaximumfloatTest2()
+        {
+            UsingFloat Usingfloat1 = new UsingFloat();
+            double result1 = Usingfloat1.Maximumfloat(3.3, 7.7, 6.6);
+            Assert.AreEqual(7.7, result1);
+        }
+        [TestMethod()]
+        public void MaximumfloatTest3()
+        {
+            UsingFloat Usingfloat2 = new UsingFloat();
+            double result2 = Usingfloat2.Maximumfloat(3.3, 7.7, 8.8);
+            Assert.AreEqual(8.8, result2);
+        }
+    }
+}
